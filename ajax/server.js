05d94a31407dc5a199030068b9eadfc76c238aa0
Thyerry Nunes <thyerry.nunes@gmail.com>
2019-03-06 21:22:31 -0300

    axios 2

--- ajax/server.js
+++ ajax/server.js
@@ -36,5 +36,17 @@ app.post('/formulario', (req, res) => {
         id: 1
     })
 })
+
+app.get('/parOuImpar', (req, res) =>{
+    // formas de receber dados do front end
+    // req.body
+    // req.query
+    // req.params
+
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
 app.get('/teste', (req, res) => res.send(new Date))
 app.listen(8080, () => console.log('Executando...'))
