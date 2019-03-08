a819334f73a9f755fb2e7792fa42a92f765a82bb
Thyerry Nunes <thyerry.nunes@gmail.com>
2019-03-07 23:00:31 -0300

    comprimindo código e tornando ele compativel com versões mais antigas

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('copiar', ['antes1', 'antes2'], () => {
         .pipe(gulp.dest('pastaB'))
 })
 
-gulp.task('antes1', () => {
+gulp.task('antes1', () =>  {
     console.log("antes 1")
 })
 
--- gulp/javascript/src/calculadora.js
+++ gulp/javascript/src/calculadora.js
@@ -24,3 +24,4 @@ const Calculadora = {
         console.log(this._resultado)
     }
 }
+
