c5ac0f0bc1fc53c741d5081259febf6f006aac1c
Thyerry Nunes <thyerry.nunes@gmail.com>
2019-03-08 22:11:41 -0300

    carregando arquivos css nos módulos webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
     mode: 'development',
     entry: './src/principal.js',
     output: {
-        filename: 'principal.js'
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader' // Interpreta @import, url()
+            ]
+        }]
     }
 }
